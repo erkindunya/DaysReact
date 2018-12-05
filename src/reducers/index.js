baef64ecd2fb486300872853b7f077cd2059092f
Enver Selchuk <enver.selchuk@kier.co.uk>
2018-12-05 12:29:17 +0000

    fix test errors

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -7,19 +7,37 @@ import {
 
 export function uiReducer(state = { isMessageDetailOpen: true }, action) {
     switch (action.type) {
+        //   case 'RECEIVE_THREAD': {
+        //       return Object.assign({}, state, {
+        //         visibilityFilter: action.filter
+        //       })
+        //     }
+        // case 'TOGGLE_MESSAGE_DETAIL': {
+        //     return Object.assign({}, state, {
+        //       visibilityFilter: action.filter
+        //     })
+        //   }
         default:
             return state
     }
 }
 
-export function threadReducer(state, action) {
+export function threadReducer(state=null, action) { // set to default action
     switch (action.type) {
-
+        case 'RECEIVE_THREAD': {
+              return action.thread;
+            }
+            default:
+            return state
     }
+
+     // in order to get /* ×
+// Error: Reducer "thread" returned undefined during initialization. If the state passed to the reducer is undefined, you must explicitly return the initial state. The initial state may not be undefined. If you don't want to set a value for this reducer, you can use null instead of undefined. */
+return state
 }
 
 export default combineReducers({
     ui: uiReducer,
-    thread: '?',
+    thread: threadReducer,
     // combineReducers can combine as many reducers as you need
 })
